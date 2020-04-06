484d71ab1e754b374388a5a52c9c7682ea82bd33
Thomas Smith (Tom) <tomytank@users.noreply.github.com>
2020-04-06 17:35:51 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# PropTypes-in-action
-Created with CodeSandbox
---
+++ src/Pokemon.js
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Pokemon(props) {
+  console.log(props);
+  return (
+    <>
+      {props.pokemon.map(pokemon => (
+        <div key={pokemon.id} className="pokemon">
+          <img src={pokemon.img} alt={pokemon.name} />
+          <div>
+            <h3>{pokemon.name}</h3>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
+
+// PropTypes to add and control
+export default Pokemon;
